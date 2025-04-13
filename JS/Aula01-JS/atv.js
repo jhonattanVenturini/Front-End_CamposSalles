d782a056faca85415e966ba59213faf87df0d1e7
JhonattanVenturini <jhonattan.venturini@outlook.com>
2025-04-13 17:04:53 -0300

    exemplo&atividade-Aula01-JS

---
+++ JS/Aula01-JS/atv.js
@@ -0,0 +1,10 @@
+
+// Atividade 1 - Criação de um sistema simples com variáveis e console.log
+
+// Baseado no arquivo main.js {Aula01-JS}, crie um sistema utilizando variáveis e o console.log 
+// para exibir informações no console do navegador. Personalize o sistema com o seu nome 
+// e implemente a lógica necessária utilizando variáveis e logs no console.
+// Sem usar o chat!
+
+// faça seu codigo aqui:
+
--- JS/Aula01-JS/main.js
+++ JS/Aula01-JS/main.js
@@ -1,6 +1,22 @@
+// Exemplo para o seu primeiro código em JavaScript
 
-// primeiro exemplo de codigo em JS - chamando uma variavel em um log
+var nome = "João"; 
+var idade = 25    
+var altura = 1.75 
+var cidade = 'São Paulo'; 
 
-let chamaNome = "Cida olá "
 
-console.log(`OLá meu amigo, ${chamaNome} tudo bem com voce?`);
+console.log('-------------------');
+console.log(`Olá, meu nome é ${nome}. , tenho ${idade} , tabém tenho ${altura}.\nEu sou estudante de ADS , moro em ${cidade}!`);
+console.log('-------------------'); 
+
+
+// Exemplo de variáveis
+// var nome = "João"; // string "texto" tudo que for entre aspas simples ou duplas
+// var idade = 25; // number numerico, inteiro ou decimal
+// var altura = 1.75; // number numerico, inteiro ou decimal
+// var cidade = 'São Paulo'; // string "texto" tudo que for entre aspas simples ou duplas
+
+//console.log('-------------------'); o texto abaixo é um exemplo de como usar o console.log para imprimir variáveis no console do navegador
+//console.log(`Olá, meu nome é ${nome}. , tenho ${idade} , tabém tenho ${altura}.\nEu sou estudante de ADS , moro em ${cidade}!`);  
+//console.log('-------------------');
