aba1386f15ee7ea8f754330e6ce7c28b489a0feb
JhonattanVenturini <jhonattan.venturini@outlook.com>
2025-07-01 23:23:31 -0300

    docs(readme): cria documentação inicial e organiza estrutura de pastas

--- Aulas/aula02-variaveis.js
+++ Aulas/aula02-variaveis.js
@@ -1,3 +1,16 @@
+let anoNascimento = 2008;
+let anoAtual = 2025;
+
+let idade = anoAtual - anoNascimento;
+
+// idade = 2025 - 2010 = 15
+
+if (idade > 18) {
+  console.log('Você é maior de idade, 😋', idade);
+} else {
+  console.log('Você é menor de idade 🤦‍♂️,', idade);
+}
+
 // constante um valor constante que não muda o mesmo até o devido momento, um dado que não pode ser varido
 
 const temperatura = 12;
